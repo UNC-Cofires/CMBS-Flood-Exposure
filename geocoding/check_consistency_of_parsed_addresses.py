4b2d39508c869f8f40914ee1a7c4ff08dd15cfba
Kieran Patrick Fitzmason (kieranf) <kpf10@pitt.edu>
2026-07-06 15:04:26 -0400

    modified approach to facilitate manual review of inconsistently parsed addresses

--- geocoding/check_consistency_of_parsed_addresses.py
+++ geocoding/check_consistency_of_parsed_addresses.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import json
 import sys
 import os
 from src.utils.config import find_project_root, load_config
@@ -53,7 +54,7 @@ loan_level_address_data = pd.read_parquet(loan_level_address_data_path)
 
 # List of LLMs whose outputs will be compared
 # (command-line argument passed as a comma-separated list)
-model_ids_string =  sys.argv[1]
+model_ids_string =  "cyankiwi/Qwen3.5-9B-AWQ-4bit,cyankiwi/gemma-4-12B-it-AWQ-INT4"#sys.argv[1]
 model_ids = model_ids_string.split(',')
 model_names = [model_id.split('/')[-1] for model_id in model_ids]
 
@@ -66,21 +67,9 @@ for model_name in model_names:
 # Load data on addresses parsed using LLMs
 data_folders = [os.path.join(pwd,f'llm_address_parsing/{model_name}') for model_name in model_names]
 parsed_addresses = pd.concat([load_chunked_parquet_data(folder) for folder in data_folders]).reset_index(drop=True)
+parsed_addresses = parsed_addresses.sort_values(by='masterloanidtrepp').reset_index(drop=True)
 
-# Create string representation of list of street numbers / addresses associated with each loan
-# (Easier to make string-to-sting comparisons than list-to-list comparisons)
-parsed_addresses['addresses_for_comparison'] = parsed_addresses['addresses'].astype('string[pyarrow]')
-parsed_addresses = parsed_addresses.sort_values(by='masterloanidtrepp')
-
-# Specify columns that will be used to check consistency across models
-comparison_cols = ['parsing_errors',
-                   'multiple_locations',
-                   'range_too_large',
-                   'range_ambiguous',
-                   'num_locations',
-                   'addresses_for_comparison']
-
-# Check whether address parsing output associated with each loan is consistent across LLMs
+comparison_cols = ['parsing_errors','address_components']
 check_consistency = parsed_addresses.groupby('masterloanidtrepp').apply(all_rows_equal,subset=comparison_cols)
 check_consistency = pd.DataFrame({'masterloanidtrepp':check_consistency.index.values,'is_consistent':check_consistency.values})
 
@@ -88,56 +77,38 @@ check_consistency = pd.DataFrame({'masterloanidtrepp':check_consistency.index.va
 consistent_loan_ids = check_consistency[check_consistency['is_consistent']==True]['masterloanidtrepp'].tolist()
 inconsistent_loan_ids = check_consistency[check_consistency['is_consistent']==False]['masterloanidtrepp'].tolist()
 
+# Create a list of loans that could not be parsed by any model
+parsing_errors_mask = (parsed_addresses['parsing_errors'])&(parsed_addresses['masterloanidtrepp'].isin(consistent_loan_ids))
+parsing_errors_loan_ids = parsed_addresses[parsing_errors_mask]['masterloanidtrepp'].unique().tolist()
+
 # Print results to console
 num_consistent_loans = len(consistent_loan_ids)
 num_inconsistent_loans = len(inconsistent_loan_ids)
 num_loans = num_consistent_loans + num_inconsistent_loans
 pct_consistent = 100*(num_consistent_loans/num_loans)
+num_parsing_failures = len(parsing_errors_loan_ids)
+pct_parsing_failures = 100*(num_parsing_failures/num_loans)
 
 print(f'\nNumber of addresses that were consistent across LLMs: {num_consistent_loans} / {num_loans} ({pct_consistent:.2f}%)')
-print(f'Number excluded because of inconsistencies across LLMs: {num_inconsistent_loans} / {num_loans} ({100-pct_consistent:.2f}%)')
-
-### *** EXCLUDE ADDRESSES THAT DON'T MEET GEOCODING CRITERIA *** ###
-
-parsed_addresses = parsed_addresses[parsed_addresses['masterloanidtrepp'].isin(consistent_loan_ids)]
-parsed_addresses = parsed_addresses.drop_duplicates(subset='masterloanidtrepp').drop(columns=['model_id','addresses_for_comparison'])
-
-m = (parsed_addresses['parsing_errors']==True)
-parsed_addresses = parsed_addresses[~m]
-print(f'Number excluded because of JSON parsing errors: {np.sum(m)} / {num_loans} ({100*np.mean(m):.1f}%)')
-
-m = (parsed_addresses['range_too_large']==True)
-parsed_addresses = parsed_addresses[~m]
-print(f'Number excluded because the range of addresses was too large: {np.sum(m)} / {num_loans} ({100*np.mean(m):.1f}%)')
-
-m = (parsed_addresses['range_ambiguous']==True)
-parsed_addresses = parsed_addresses[~m]
-print(f'Number excluded because the range of addresses was ambiguous: {np.sum(m)} / {num_loans} ({100*np.mean(m):.1f}%)')
-
-### *** CREATE FINAL LIST OF LOCATIONS TO GEOCODE *** ###
-
-# Explode dataframe so that each row corresponds to a potential location associated with a loan.
-# Loans whose original addresses correspond to multiple locations (e.g., "110-115 Main St")
-# will now show up multiple times in the dataframe (e.g., "110 Main St","111 Main St",...,"115 Main St"). 
-parsed_addresses = parsed_addresses[['masterloanidtrepp','address_string','addresses']].explode('addresses')
-parsed_addresses = parsed_addresses.rename(columns={'address_string':'original_address_string','addresses':'parsed_address'})
-parsed_addresses = parsed_addresses.reset_index(drop=True)
-
-# Attach data on city / state / zip
-parsed_addresses = pd.merge(parsed_addresses,loan_level_address_data,how='left',on='masterloanidtrepp')
-parsed_addresses = parsed_addresses[['masterloanidtrepp','propname','original_address_string','parsed_address','city','state','zip']]
-
-# Print update to console
-final_num_loans = len(parsed_addresses['masterloanidtrepp'].unique())
-final_num_addresses = len(parsed_addresses)
-
-print(f'Final list of locations to geocode consists of {final_num_addresses} addresses associated with {final_num_loans} unique loans.')
-print(f'These loans represent {final_num_loans} / {num_loans} ({100*(final_num_loans/num_loans):.2f}%) of the pre-parsing number of loans.')
+print(f'Number of addresses that were inconsistent across LLMs: {num_inconsistent_loans} / {num_loans} ({100-pct_consistent:.2f}%)')
+print(f'Number of addresses that were consistent but could not be parsed: {num_parsing_failures} / {num_loans} ({pct_parsing_failures:.2f}%)')
 
 ### *** SAVE RESULTS *** ###
 
-outname = os.path.join(address_dir,'parsed_loan_addresses.parquet')
-parsed_addresses.to_parquet(outname)
+# Split out addresses that were inconsistent or that failed to parse for manual review
+manual_review_mask = parsed_addresses['masterloanidtrepp'].isin(inconsistent_loan_ids)|parsed_addresses['masterloanidtrepp'].isin(parsing_errors_loan_ids)
+manual_review_addresses = parsed_addresses[manual_review_mask].reset_index(drop=True)
+consistent_addresses = parsed_addresses[~manual_review_mask].drop(columns='model_id').drop_duplicates().reset_index(drop=True)
 
-outname = os.path.join(address_dir,'parsing_consistency_check_results.parquet')
-check_consistency.to_parquet(outname)
+# Randomly sample 500 addresses from consistent sample to be manually reviewed for correctness
+consistent_review_sample = consistent_addresses.sample(500)
+
+# Save to file
+outname = os.path.join(address_dir,'consistent_parsed_addresses.parquet')
+consistent_addresses.to_parquet(outname)
+
+outname = os.path.join(address_dir,'consistent_parsed_addresses_random_sample.parquet')
+consistent_addresses.to_parquet(outname)
+
+outname = os.path.join(address_dir,'manual_review_parsed_addresses.parquet')
+consistent_addresses.to_parquet(outname)
