85251604db3ccee1bea34aadfc03dfbfb7f41f2f
Kieran Patrick Fitzmason (kieranf) <kpf10@pitt.edu>
2026-08-05 10:38:47 -0400

    stopped tracking files from approaches that were a dead end

--- manuscript/.Rprofile
+++
@@ -1,2 +0,0 @@
-Sys.setenv(RENV_PATHS_CACHE = "/users/k/i/kieranf/renv_cache")
-source("renv/activate.R")
--- manuscript/render_quarto_pdf.sh
+++ manuscript/render_quarto_pdf.sh
@@ -3,19 +3,20 @@
 #SBATCH -p general
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=8g
-#SBATCH -t 3-00:00:00
+#SBATCH --mem=32g
+#SBATCH -t 0-01:00:00
+#SBATCH --output=logs/render-%j.log
 #SBATCH --mail-type=all
-#SBATCH --job-name=search_mediacloud
+#SBATCH --job-name=render_quarto
 #SBATCH --mail-user=kieranf@email.unc.edu
 
 module purge
-module load anaconda
 
 export PYTHONWARNINGS="ignore"
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 source "$PROJECT_ROOT/config.sh"
 
-conda activate $CRE_CONDA_ENV_PATH
+module load positron/2026.05.2-3
 
-python3.12 search_mediacloud.py
+quarto render --to pdf
+
