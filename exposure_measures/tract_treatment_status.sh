6953bc5c1e6e86598312f0fa03d65b797f40a12a
Kieran Patrick Fitzmason (kieranf) <kpf10@pitt.edu>
2026-09-22 17:09:01 -0400

    updated treatment thresholds

--- exposure_measures/tract_treatment_status.sh
+++ exposure_measures/tract_treatment_status.sh
@@ -18,8 +18,8 @@ source "$PROJECT_ROOT/config.sh"
 
 conda activate $CRE_CONDA_ENV_PATH
 
-python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.10 5 "base_case"
-python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.20 5 "higher_threshold"
-python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.05 5 "lower_threshold"
-python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.10 10 "longer_duration"
-python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.10 3 "shorter_duration"
+python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.20 5 "base_case"
+python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.40 5 "higher_threshold"
+python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.10 5 "lower_threshold"
+python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.20 10 "longer_duration"
+python3.12 tract_treatment_status.py "NFIP_claim_rate_by_tract.parquet" 0.20 3 "shorter_duration"
